88c9e98a6ad95f31660cc2e835a908f7e4b1f3b3
Rex <rex@mail.leafwolf.net>
2021-07-15 23:44:09 +0800

    update

--- includes/CommonCtil.h
+++ includes/CommonCtil.h
@@ -20,6 +20,10 @@
 #define CMT_ENV_WINDOWS
 #elif __APPLE__
 #define CMT_ENV_APPLE
+#elif __arm__
+#define CMT_ENV_ARM
+#elif i386
+#define CMT_ENV_I386
 #endif
 
 //程序位数
@@ -37,8 +41,8 @@
 #endif
 
 //常用数据
-#define CMT_FLOAT_TOL 0.00001
-#define CMT_PI 3.141592653589793
+#define CMT_FLOAT_TOL		 0.00001
+#define CMT_PI				 3.141592653589793
 #define TRUE 1
 #define FALSE 0
 
@@ -46,4 +50,34 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
+
+//定义CommonCtil数据类型
+typedef unsigned char         cmtU8;
+typedef unsigned short        cmtU16;
+typedef unsigned int          cmtU32;
+typedef char                  cmtS8;
+typedef short                 cmtS16;
+typedef int                   cmtS32;
+
+#if defined(CMT_ENV_I386) || defined(CMT_ENV_ARM) || defined(CMT_ENV_WINDOWS)
+typedef unsigned long long    cmtU64;
+typedef long long             cmtS64;
+#else
+typedef unsigned long         cmtU64;
+typedef long                  cmtS64;
+#endif
+typedef unsigned char*		  cmtU8ptr;
+typedef unsigned short*       cmtU16ptr;
+typedef unsigned int*         cmtU32ptr;
+typedef char*				  cmtS8ptr;
+typedef short*				  cmtS16ptr;
+typedef int*                  cmtS32ptr;
+
+#if defined(CMT_ENV_I386) || defined(CMT_ENV_ARM) || defined(CMT_ENV_WINDOWS)
+typedef long long*            cmtS64ptr;
+typedef unsigned long long*   cmtU64ptr;
+#else
+typedef long*                 cmtS64ptr;
+typedef unsigned long*		  cmtU64ptr;
+#endif
 #endif
