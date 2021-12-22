be20c47cca868efef6f186b43aa9b0141e5ed3c5
dexnab <youremail@example.com>
2021-12-22 12:23:01 +0800

    ftu_string

--- src/cmtString.h
+++ src/cmtString.h
@@ -901,6 +901,19 @@ extern cmtUint64 cmtSprintfBin(cmtU8str* out, cmtFmtInfo* info, cmtUint64 arg);
 */
 extern cmtUint64 cmtSprintfDec(cmtU8str* out, cmtFmtInfo* info, cmtInt64 arg);
 
+/**
+* @brief 整数转字符串（八进制模式）
+* @param[out] out 生成的字符串
+* @param[in] info 格式
+* @param[in] arg 整数
+* @pre @a out 字符串的size字段必须填写，以限制输出缓冲区大小
+* @see doc/格式化字符串.md
+* @test cmtDemoSprintfOct() 状态：NC
+* @date 2021-12-22
+* @author dexnab
+*/
+extern cmtUint64 cmtSprintfOct(cmtU8str* out, cmtFmtInfo* info, cmtUint64 arg);
+
 //status: NC
 extern cmtUint64 cmtSprintfSize(cmtU8str* format, ...);
 
